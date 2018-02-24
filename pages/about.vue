6501c572c060693f68885793952e3411ffc3519a
selvesan <dev.selvesan@gmail.com>
2018-02-24 14:47:58 +0545

    push

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
         <div class="container has-text-centered">
             <h1 class="title">About Me</h1>
             <p>
-                My name is selvesan, I’m a self taught software developer. I build modern applications for the web!.
+                My name is selvesan, I’m a software developer. I build modern applications for the web!.
             </p>
         </div>
     </section>
--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
         <div class="container has-text-centered">
             <h1 class="title">Contact Me</h1>
             <p>
-                You can follow me on Twitter: <a href="https://twitter.com/selvesan" target="_blank">@selvesan</a>
+                You can follow me on Twitter: <a href="https://twitter.com/selves007" target="_blank">@selves007</a>
             </p>
         </div>
     </section>
